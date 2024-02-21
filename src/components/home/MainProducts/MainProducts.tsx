ad6f801e728e8e8f97e1908de0898ef7d715b6f4
Xavi Flores <xa_franc@live.com>
2024-02-21 16:04:58 -0600

    css module next

---
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -0,0 +1,3 @@
+export const MainProducts = () => {
+  return <h1>MainProducts</h1>;
+};
---
+++ src/components/shared/Header/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/store">
+            <li>Store</li>
+          </Link>
+          <Link href="/store/example">
+            <li>Example</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
